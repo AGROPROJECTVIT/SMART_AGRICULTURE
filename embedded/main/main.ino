b1053e9976585df933a93e51875b1f693f1afe84
PrathameshBakare <prathamesh.1252100014@vit.edu>
2026-03-01 20:21:22 +0530

    Update main.ino

--- embedded/main/main.ino
+++ embedded/main/main.ino
@@ -4,65 +4,54 @@
 /*==================================================
                 PIN DEFINITIONS
 ==================================================*/
-#define GSM_PWR   PB_0
+#define GSM_PWR      PB_0
+#define RELAY_PIN    PB_5
 
 
 /*==================================================
-                FUNCTION PROTOTYPES
+                GSM UART
 ==================================================*/
+HardwareSerial gsmSerial(PA_10, PA_9);
+
+
+/*==================================================
+                GLOBAL VARIABLES
+==================================================*/
+String receivedSMS = "";
+int valveDuration = 0;
+
+
+/*==================================================
+            FUNCTION DECLARATIONS
+==================================================*/
+void System_Init(void);
 void GSM_PowerON(void);
 void GSM_PowerOFF(void);
-void enterSleep(uint32_t time_ms);
+void GSM_SendCommand(const char *cmd);
+void GSM_InitSMS(void);
+void GSM_ReadSMS(void);
+void GSM_ParseSMS(void);
+void Valve_ON(void);
+void Valve_OFF(void);
+void OperateValve(int minutes);
+void System_Sleep(uint32_t sleepTime);
 
 
 /*==================================================
-                GSM POWER ON
+                SYSTEM INITIALIZATION
 ==================================================*/
-void GSM_PowerON(void)
-{
-    Serial.println("GSM POWER ON");
-
-    digitalWrite(GSM_PWR, HIGH);
-
-    delay(5000);   // GSM boot time
-}
-
-
-/*==================================================
-                GSM POWER OFF
-==================================================*/
-void GSM_PowerOFF(void)
-{
-    Serial.println("GSM POWER OFF");
-
-    digitalWrite(GSM_PWR, LOW);
-}
-
-
-/*==================================================
-                LOW POWER MODE
-==================================================*/
-void enterSleep(uint32_t time_ms)
-{
-    Serial.println("Entering Sleep Mode");
-
-    LowPower.deepSleep(time_ms);
-
-    Serial.println("Wakeup Complete");
-}
-
-
-/*==================================================
-                    SETUP
-==================================================*/
-void setup(void)
+void System_Init(void)
 {
     Serial.begin(9600);
     delay(2000);
 
     pinMode(GSM_PWR, OUTPUT);
+    pinMode(RELAY_PIN, OUTPUT);
 
-    GSM_PowerOFF();
+    digitalWrite(GSM_PWR, LOW);
+    digitalWrite(RELAY_PIN, LOW);
+
+    gsmSerial.begin(9600);
 
     LowPower.begin();
 
@@ -71,15 +60,172 @@ void setup(void)
 
 
 /*==================================================
+                GSM POWER CONTROL
+==================================================*/
+void GSM_PowerON(void)
+{
+    Serial.println("GSM POWER ON");
+    digitalWrite(GSM_PWR, HIGH);
+    delay(6000);
+}
+
+void GSM_PowerOFF(void)
+{
+    Serial.println("GSM POWER OFF");
+    digitalWrite(GSM_PWR, LOW);
+}
+
+
+/*==================================================
+                GSM COMMAND
+==================================================*/
+void GSM_SendCommand(const char *cmd)
+{
+    gsmSerial.println(cmd);
+    delay(1000);
+}
+
+
+/*==================================================
+                GSM SMS INIT
+==================================================*/
+void GSM_InitSMS(void)
+{
+    GSM_SendCommand("AT");
+    GSM_SendCommand("AT+CMGF=1");
+    GSM_SendCommand("AT+CNMI=2,2");
+}
+
+
+/*==================================================
+                VALVE CONTROL
+==================================================*/
+void Valve_ON(void)
+{
+    Serial.println("VALVE ON");
+    digitalWrite(RELAY_PIN, HIGH);
+}
+
+void Valve_OFF(void)
+{
+    Serial.println("VALVE OFF");
+    digitalWrite(RELAY_PIN, LOW);
+}
+
+
+/*==================================================
+            OPERATE VALVE TIMER
+==================================================*/
+void OperateValve(int minutes)
+{
+    Valve_ON();
+
+    for(int i=0;i<minutes;i++)
+    {
+        delay(60000);     // 1 minute
+    }
+
+    Valve_OFF();
+}
+
+
+/*==================================================
+                SMS PARSER
+==================================================*/
+void GSM_ParseSMS(void)
+{
+    int index;
+
+    /* USER COMMAND */
+    if(receivedSMS.indexOf("VALVE ON") >= 0)
+    {
+        index = receivedSMS.lastIndexOf(" ");
+
+        valveDuration =
+            receivedSMS.substring(index+1).toInt();
+
+        Serial.print("Running Valve for ");
+        Serial.print(valveDuration);
+        Serial.println(" minutes");
+
+        OperateValve(valveDuration);
+    }
+
+    if(receivedSMS.indexOf("VALVE OFF") >= 0)
+    {
+        Valve_OFF();
+    }
+
+    /* LOAD SHEDDING MESSAGE */
+    if(receivedSMS.indexOf("LOAD") >= 0)
+    {
+        Serial.println("LOAD SHEDDING DETECTED");
+
+        OperateValve(10);   // default action
+    }
+
+    receivedSMS="";
+}
+
+
+/*==================================================
+                READ GSM DATA
+==================================================*/
+void GSM_ReadSMS(void)
+{
+    char rx;
+
+    while(gsmSerial.available())
+    {
+        rx = gsmSerial.read();
+
+        Serial.write(rx);
+
+        receivedSMS += rx;
+
+        if(rx=='\n')
+        {
+            GSM_ParseSMS();
+        }
+    }
+}
+
+
+/*==================================================
+                LOW POWER MODE
+==================================================*/
+void System_Sleep(uint32_t sleepTime)
+{
+    GSM_PowerOFF();
+
+    Serial.println("ENTERING LOW POWER");
+
+    LowPower.deepSleep(sleepTime);
+
+    Serial.println("WAKEUP");
+}
+
+
+/*==================================================
+                    SETUP
+==================================================*/
+void setup(void)
+{
+    System_Init();
+
+    GSM_PowerON();
+
+    GSM_InitSMS();
+}
+
+
+/*==================================================
                     MAIN LOOP
 ==================================================*/
 void loop(void)
 {
-    GSM_PowerON();
+    GSM_ReadSMS();
 
-    delay(10000);      // GSM active time
-
-    GSM_PowerOFF();
-
-    enterSleep(15000); // sleep 15 sec
+    /* Sleep after checking SMS */
+    System_Sleep(30000);   // sleep 30 sec
 }
